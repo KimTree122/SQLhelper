e22010542e8b7cf7448223c236fc3bddc2f73c69
kimtree122 <839414748@qq.com>
2018-10-11 09:14:37 +0800

    增加结构struct例子 可用于方法构建件。后进先出

---
+++ KNDBsys.Common/TestTools/StructSample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KNDBsys.Common.TestTools
+{
+    public  struct StructSample
+    {
+        public int X;
+        public int Y;
+        
+        public  StructSample(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static StructSample operator +(StructSample s1, StructSample s2)
+        {
+
+            return new StructSample(s1.X + s2.X, s1.Y + s2.Y);
+        }
+
+        public static implicit operator int(StructSample ss)
+        {
+            return (int)(ss.X + ss.Y);
+        }
+
+        public override string ToString()
+        {
+            return ($"({X},{Y})");
+        }
+    }
+
+    public  class StructTest
+    {
+        
+
+        public void MyTest()
+        {
+            StructSample ss1 = new StructSample(1, 2);
+            StructSample ss2 = new StructSample(2, 3);
+
+            int count = ss1 + ss2;
+        }
+
+
+    }
+
+}
